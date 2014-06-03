3070769763cfd37e41a812ce7b4d1a917a1f0c28
Mike Flynn <mfflynn@gmail.com>
2014-06-03 11:18:50 -0700

    master: Added urls to project file.

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject beats-clj "0.1.0"
   :description "A Clojure library to interact with the Beats Music API."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/mikeflynn/beats-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [cheshire "5.3.1"]
                  [http-kit "2.1.16"]]
-  :plugins [[codox "0.8.8"]])
+  :plugins [[codox "0.8.8"]]
+  :codox {:src-dir-uri "https://github.com/mikeflynn/beats-clj/tree/master"})
