393cb1a720463efc5589a0c9f7ae03a5535c7a2f
Mike Flynn <mfflynn@gmail.com>
2014-06-03 16:55:39 -0700

    master: Updated url to point to gh-page rather than the github project page.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject beats-clj "0.8.0"
   :description "A Clojure library to interact with the Beats Music API."
-  :url "https://github.com/mikeflynn/beats-clj"
+  :url "https://mikeflynn.github.io/beats-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
